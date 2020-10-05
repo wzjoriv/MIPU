961cb00d52c16c2d1d8eded45c511359da7f3f4f
Josue N Rivera <josue.n.rivera@outlook.com>
2020-10-05 09:59:47 -0400

    Patch for MIPU's minimum pump

--- src/main/MinPumpingLength.java
+++ src/main/MinPumpingLength.java
@@ -83,7 +83,7 @@ public class MinPumpingLength {
         			if(pump(xx, yy, zz)) {
         				if(minString == null) { //if it can be pumped and their is not a smaller string than it, make this minimum string
         					minString = nextstr;
-        					minp = prevstr.length() + 1;
+        					minp = xy.length();
         					
         					this.x = xx;
         					this.y = yy;
